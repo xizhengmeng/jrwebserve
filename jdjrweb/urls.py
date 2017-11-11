2b5af26ebdb0aa34b7dc8ac607b407acf348a5db
hanshenghui <jrmacpro@jritdeMacBook-Pro-3.local>
2017-11-11 15:17:08 +0800

    添加初始化代码

--- jdjrweb/urls.py
+++ jdjrweb/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from jrweb import views as jrviews  # new
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'test', jrviews.test),
+    # url(r'^$', jrviews.home,name='home'),  # new
 ]
