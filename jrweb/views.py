1fb15cf57830ad00084d42a2880cab3a8073e66a
hanshenghui <jrmacpro@jritdeMacBook-Pro-3.local>
2017-11-11 15:18:35 +0800

    增加测试数据

--- jrweb/views.py
+++ jrweb/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
+import os,datetime,json
 from __future__ import unicode_literals
-
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
