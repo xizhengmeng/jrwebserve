5ffd1bc75d1012b9aecf71836cd61a3951922aa5
hanshenghui <jrmacpro@jritdeMacBook-Pro-3.local>
2017-11-13 11:11:18 +0800

    ceshi

--- jrweb/views.py
+++ jrweb/views.py
@@ -12,15 +12,26 @@ def test(request):
     return HttpResponse("ceshiceshi")
 
 def searchCommentFunction(request):
-    # searchkey = request.POST.get('searchkey')
-    # sorekey = request.POST.get('sorekey')
-    # pageindex = request.POST.get('pageindex')
-    # pagesize = request.POST.get('pagesize')
+    postObj = request.POST()
+    getObj = request.GET()
 
-    searchkey = request.GET.get('searchkey')
-    sorekey = request.GET.get('sorekey')
-    pageindex = request.GET.get('pageindex')
-    pagesize = request.GET.get('pagesize')
+    searchkey = ''
+    sorekey = ''
+    pageindex = 0
+    pagesize = 0
+
+    if type(postObj) != type(None):
+       searchkey = request.POST.get('searchkey')
+       sorekey = request.POST.get('sorekey')
+       pageindex = request.POST.get('pageindex')
+       pagesize = request.POST.get('pagesize')
+    elif type(getObj) != type(None):
+       searchkey = request.GET.get('searchkey')
+       sorekey = request.GET.get('sorekey')
+       pageindex = request.GET.get('pageindex')
+       pagesize = request.GET.get('pagesize')
+    else:
+       return HttpResponse('no request body')
 
     searchResultString = searchComment(searchkey,sorekey,pageindex,pagesize)
     
