0dcb10381bde085221ae0de4109f7a8c5ef306e2
root <root@vultr.guest>
2017-11-13 14:10:41 +0000

    ceshi

--- spider/tutorial/pipelines.py
+++ spider/tutorial/pipelines.py
@@ -33,6 +33,7 @@ class TutorialPipeline(object):
               raise DropItem("Duplicate item found: %s" % item)
            else:
               item['storename'] = 'iosappstore'
+              print item['body']
               postItem = dict(item)  # 把item转化成字典形式
               self.coll.insert(postItem)  # 向数据库插入一条记录
         elif spider.name == 'andorid':
