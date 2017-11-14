1b71ab4c177ad5bfb6774e012aa7bc49ed931a9b
hanshenghui <jrmacpro@jritdeMacBook-Pro-3.local>
2017-11-14 17:54:36 +0800

    ceshi

--- corefiles/cleandata.py
+++ corefiles/cleandata.py
@@ -8,6 +8,36 @@ import re
 reload(sys)
 sys.setdefaultencoding( "utf-8" )
 
+def checkIfisuseful(string):
+    if len(body) > 10 and len(body) < 150:
+        unitext = unicode(string, 'utf-8')
+        stringlist = unitext
+        dic = {}
+        for item in stringlist:
+            count = dic.get(item)
+            if type(count) == type(None):
+                count = 1
+                dic[item] = count
+            else:
+                count = count + 1
+                dic[item] = count
+        keys = dic.keys()
+        total = 0
+        for key in keys:
+            count = dic.get(key)
+            total = count + total
+        for key in keys:
+            count = dic.get(key) * 1.0
+            percent = count / total
+            if percent > 0.3:
+                print key, percent
+                print string
+                return False
+        return True
+
+    else:
+        return False
+
 connection = pymongo.MongoClient("localhost",27017)
 mydb = connection.Spider # new a database
 comment = mydb.Comment # new a table
@@ -28,10 +58,10 @@ for item in dbs:
     if userReviewId in idList:
         continue
     body = item.get('body')
-    if len(body) > 10 and len(body) < 150 and 1.0*body.count('好')/len(body)*3 < 0.4:
-        print 'clencomment insert new'
-        print body
-        print item['date']
-    	clencomment.insert(item)
+    if checkIfisuseful(body) == True:
+       print 'clencomment insert new'
+       print body
+       print item['date']
+       clencomment.insert(item)
 
 
