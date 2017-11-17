d4a121428781747852b165a17b7f0763d9ddc2a7
hanshenghui <jrmacpro@jritdeMacBook-Pro-3.local>
2017-11-17 13:47:41 +0800

    ceshi

--- corefiles/analysesentence.py
+++ corefiles/analysesentence.py
@@ -8,25 +8,34 @@ mydb = connection.Spider  # new a database
 basedata = mydb.analysebasedata  # new a table
 clencomment = mydb.CleanComment
 
+basedata = mydb.analysebasedata  # new a table
+
+basedbs = basedata.find({})
+
+neglist = []
+poslist = []
+neulist = []
+
+for item in basedbs:
+    poslist = item['pos']
+    neglist = item['neg']
+
 dbs = clencomment.find({})
 analyseList = []
 
 for item in dbs:
     nominal = 0
-    if len(item['body']) > 30 or int(item['rating']) > 3:
+    if len(item['forsearch']) > 30 or int(item['rating']) > 3:
         continue
     seg_list = jieba.cut(item['forsearch'])
     print '**************' + '%d' % item['rating']
-    print item['forsearch']
+    # print item['forsearch']
     for word in seg_list:
-        print word
-    baseitems = basedata.find({'title': word})
-    if baseitems.count() > 0:
-        baseitem = baseitems[0]
-        itemnominal = baseitem.get('nominal')
-        nominal = nominal + itemnominal
-        print itemnominal, word
-    print item['body'],nominal
+        if word in poslist:
+           nominal = nominal - 1
+        elif word in neglist:
+           nominal = nominal + 1
+    print item['forsearch'],nominal
 
 #clencomment.remove({})
 
