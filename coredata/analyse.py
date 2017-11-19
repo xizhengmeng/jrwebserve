9128bd91e113691aad9598f50d682ecece32a199
hanshenghui <362640516@qq.com>
2017-11-19 18:16:44 +0800

    ceshi

--- coredata/analyse.py
+++ coredata/analyse.py
@@ -8,10 +8,14 @@ sys.setdefaultencoding( "utf-8" )
 
 items = open('../coredata/fordelete.txt').read().split('\n')
 
-print len(items)
+itemnews = open('../coredata/neg.txt').read().split('\n')
 
-itemnews = open('../coredata/neg.txt').read().split('\r')
-
+count = 0
 for item in itemnews:
     if item not in items:
        print item
+       count = count + 1
+
+print len(itemnews)
+print count
+print len(items)
